70674d0f9bc89abab6e24331c156f2723261b616
junya-1 <77085068+junya-1@users.noreply.github.com>
2021-02-10 09:17:56 +0900

    Update entrypoint.sh

--- action-a/entrypoint.sh
+++ action-a/entrypoint.sh
@@ -1,11 +1,16 @@
 #!/bin/sh -l
 
+comment_and_message()
+{
+    echo "================================================================================"
+    echo "$1"
+    echo "================================================================================"
+    $1
+}
+
 sh -c "echo Hello world my name is $INPUT_MY_NAME"
 
-uname -a
-
-df -h
-
-cat /proc/cpuinfo
-
-cat /etc/os-release
+comment_and_message "uname -a"
+comment_and_message "df -h"
+comment_and_message "cat /proc/cpuinfo"
+comment_and_message "cat /etc/os-release"
